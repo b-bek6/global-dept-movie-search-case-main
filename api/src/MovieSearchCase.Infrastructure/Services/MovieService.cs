fcc91589df6d86d4ca27fe217603c1d2d6f8d940
Bibek Ghimire <binekghimire555@gmail.com>
2026-09-15 19:48:40 +0545

    feat: implement search method in MovieService to query TMDB

--- api/src/MovieSearchCase.Infrastructure/Services/MovieService.cs
+++ api/src/MovieSearchCase.Infrastructure/Services/MovieService.cs
@@ -17,4 +17,8 @@ public class MovieService : IMovieService
         _tmdbClient.GetTrendingMoviesAsync(cancellationToken);
 
     // TODO(candidate): add a search method here that calls ITmdbClient's new search method.
+    public Task<PagedResponse<Movie>> SearchAsync(string query, int page, CancellationToken cancellationToken)
+    {
+        return _tmdbClient.SearchMoviesAsync(query, page, cancellationToken);
+    }
 }
